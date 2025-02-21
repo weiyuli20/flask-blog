f832a9173bf270b2201b7be1eacede9224377db9
liweiyu <liqing20@mails.ucas.ac.cn>
2025-02-21 22:37:32 +0800

    使用模版template进行页面渲染

--- notes.md
+++ notes.md
@@ -2,6 +2,7 @@
 - windows可以在终端设置 set FLASK_APP=blog.py
 然后执行 flask run 启动服务器吗，这种方法打开新的终端时，需要重新设置
 - 全局设置方式: pip install python-dotenv,然后在项目根目录下创建 .flaskenv 文件，里面设置环境变量，FLASK_APP=blog.py,然后执行 flask run 启动服务器
+- https://www.cnblogs.com/yoyoketang/p/16631599.html
 
 ## 2、关于__init__.py文件
 https://zhuanlan.zhihu.com/p/115350758
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,20 @@
 from src import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "hello world!"
+    user = {'username': 'Miguel'}
+    posts = [
+        { 
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        { 
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
+    
+    
