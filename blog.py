a4c44ea8dca23e46ae4715cc0cc8ba31cad2ea17
liweiyu <liqing20@mails.ucas.ac.cn>
2025-02-23 16:10:20 +0800

    注册flask shell 上下文

--- blog.py
+++ blog.py
@@ -1,1 +1,6 @@
-from src import app
+from src import app,db
+from src.models import User,Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'app': app, 'db': db,'User':User,'Post':Post}
