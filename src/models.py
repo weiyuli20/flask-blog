c07f0dcc2340d71f695e7151edc7e50266564b3c
liweiyu <liqing20@mails.ucas.ac.cn>
2025-02-24 22:28:50 +0800

    粉丝功能

--- src/models.py
+++ src/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from src import login
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 #Flask-SQLAlchemy自动设置类名为小写来作为对应表的名称
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key = True)
@@ -14,6 +19,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post',backref='author',lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime,default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 
     def set_password(self,password):
@@ -31,6 +41,25 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
     def __repr__(self):
