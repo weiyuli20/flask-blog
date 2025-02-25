c624bb62d5be7f03cc86ec96fbac219eed2940da
weiyu <liqing20@mails.ucas.ac.cn>
2025-02-25 11:23:41 +0800

    分页功能

--- config.py
+++ config.py
@@ -17,3 +17,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+
+    #配置分页
+    POSTS_PER_PAGE = 10
--- src/routes.py
+++ src/routes.py
@@ -25,8 +25,15 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))  #Post/Redirect/Get 重定向向浏览器返送302响应，浏览器会重新发起get请求
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title = 'Home', form=form,posts = posts)
+    
+    page = request.args.get('page', 1, type=int)  #可以从page查询字符串参数获得或是默认值1
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
+    )
+    next_url = url_for('index', page = posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page = posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title = 'Home', form=form,posts = posts.items, next_url = next_url, prev_url = prev_url)
 
 @app.route("/register",methods=['GET', 'POST'])
 def register():
@@ -59,7 +66,6 @@ def login():
         if not next_page or url_parse(next_page).netloc != '': # next_page 为空或者它是一个外部链接
             next_page = url_for('index')
         return redirect(next_page)
-        return redirect(url_for('index'))
     return render_template('login.html', title = 'Sign In', form = form)
 
 @app.route("/logout")
@@ -71,11 +77,15 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404() #获取不到用户时，返回404
-    posts=[
-        {'author':user,'body':'Test post #1'},
-        {'author':user,'body':'Test post #2'}
-    ]
-    return render_template('user.html',user=user,posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route("/edit_profile",methods=['GET', 'POST'])
@@ -125,5 +135,13 @@ def unfollow(username):
 @app.route("/explore")
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title = 'Explore',posts = posts)
+    page = request.args.get('page',1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
+    )
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
